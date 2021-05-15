639411ea4f6e832720deff5fb86ea19f39ce27f3
AdamCSmithCWS <adam.smith2@canada.ca>
2021-05-15 15:46:20 -0400

    working trend model with season adjust

--- Data_prep.R
+++ Data_prep.R
@@ -6,20 +6,20 @@
 library(bbsBayes)
 library(tidyverse)
 library(mgcv)
-library(cmdstanr)
 
 
-
+source("Functions/GAM_basis_function_mgcv.R")
 
 # prep the BBS data -------------------------------------------------------
 
-st_dat = stratify(by = "bbs_usgs")
+st_dat = stratify(by = "bbs_cws")
 
 species = "Allen's Hummingbird"
 
 
 spsData = prepare_jags_data(st_dat,species_to_run = species,
-                            model = "slope")
+                            model = "gamye",
+                            heavy_tailed = TRUE)
 
 
 #require(lubridate)
@@ -66,6 +66,41 @@ bp = ggplot(data = tmp,aes(y = lcount,x = doy,colour = yr_d))+
 print(bp)
 
 
+spsData$season <- spsData$doy-(min(spsData$doy)-1)
+
+nseason= max(spsData$season)
+
+season_gam = gam_basis(orig.preds = 1:nseason,
+                    nknots = floor(nseason/7),
+                    npredpoints = nseason,
+                    sm_name = "season")
+spsData$nseason = nseason
+
+# bbsBayes::model_to_file(model = "gamye",
+#                         filename = "models/gamye_season_blank.R",
+#                         heavy_tailed = TRUE)
+
+spsData$season_basis <- season_gam$season_basis
+spsData$nknots_season <- season_gam$nknots_season
+spsData$decade <- as.integer(spsData$decadeF)
+spsData$ndecades <- max(spsData$decade)
+
+# names(spsData)
+
+parms = c("n",
+          "n3",
+          "seasoneffect",
+          "beta_season",
+          "B_season",
+          "tau_B_season",
+          "taubeta_season")
+
+spsData$decadeF <- NULL
+ spsData$ndecade <- NULL
+# spsData$decadeF <- NULL
 
 
-
+fit <- bbsBayes::run_model(jags_data = spsData,
+                           model_file_path = "models/gamye_season.R",
+                           parameters_to_save = parms,
+                           parallel = TRUE)
